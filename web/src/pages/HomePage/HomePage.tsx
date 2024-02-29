bd326ac30b6187e3f7ecf1d94159a9f4fa1608d7
Jake Hayes <thejayhaykid@users.noreply.github.com>
2024-02-29 06:29:58 -0500

    Better looking homepage

--- web/src/pages/HomePage/HomePage.tsx
+++ web/src/pages/HomePage/HomePage.tsx
@@ -1,20 +1,22 @@
+import { ChevronRightIcon } from '@heroicons/react/24/outline'
+
 import { Link, routes } from '@redwoodjs/router'
 
 import HomeLayout from 'src/layouts/HomeLayout/HomeLayout'
 
 const HomePage = () => {
   return (
-    <div className="bg-primary">
+    <div className="min-h-full bg-primary">
       <HomeLayout>
         <main>
-          <div className="relative isolate">
+          <div className="relative isolate overflow-hidden">
             <svg
-              className="stroke-gray-200 absolute inset-x-0 top-0 -z-10 h-[64rem] w-full [mask-image:radial-gradient(32rem_32rem_at_center,white,transparent)]"
+              className="stroke-white/10 absolute inset-0 -z-10 h-full w-full [mask-image:radial-gradient(100%_100%_at_top_right,white,transparent)]"
               aria-hidden="true"
             >
               <defs>
                 <pattern
-                  id="1f932ae7-37de-4c0a-a8b0-a6e3b4d44b84"
+                  id="983e3e4c-de6d-4c3f-8d64-b9761d1534cc"
                   width={200}
                   height={200}
                   x="50%"
@@ -24,7 +26,7 @@ const HomePage = () => {
                   <path d="M.5 200V.5H200" fill="none" />
                 </pattern>
               </defs>
-              <svg x="50%" y={-1} className="fill-gray-50 overflow-visible">
+              <svg x="50%" y={-1} className="fill-gray-800/20 overflow-visible">
                 <path
                   d="M-200 0h201v201h-201Z M600 0h201v201h-201Z M-400 600h201v201h-201Z M200 800h201v201h-201Z"
                   strokeWidth={0}
@@ -34,102 +36,79 @@ const HomePage = () => {
                 width="100%"
                 height="100%"
                 strokeWidth={0}
-                fill="url(#1f932ae7-37de-4c0a-a8b0-a6e3b4d44b84)"
+                fill="url(#983e3e4c-de6d-4c3f-8d64-b9761d1534cc)"
               />
             </svg>
             <div
-              className="absolute left-1/2 right-0 top-0 -z-10 -ml-24 transform-gpu overflow-hidden blur-3xl lg:ml-24 xl:ml-48"
+              className="absolute left-[calc(50%-4rem)] top-10 -z-10 transform-gpu blur-3xl sm:left-[calc(50%-18rem)] lg:left-48 lg:top-[calc(50%-30rem)] xl:left-[calc(50%-24rem)]"
               aria-hidden="true"
             >
               <div
-                className="aspect-[801/1036] w-[50.0625rem] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30"
+                className="aspect-[1108/632] w-[69.25rem] bg-gradient-to-r from-[#80caff] to-[#4f46e5] opacity-20"
                 style={{
                   clipPath:
-                    'polygon(63.1% 29.5%, 100% 17.1%, 76.6% 3%, 48.4% 0%, 44.6% 4.7%, 54.5% 25.3%, 59.8% 49%, 55.2% 57.8%, 44.4% 57.2%, 27.8% 47.9%, 35.1% 81.5%, 0% 97.7%, 39.2% 100%, 35.2% 81.4%, 97.2% 52.8%, 63.1% 29.5%)',
+                    'polygon(73.6% 51.7%, 91.7% 11.8%, 100% 46.4%, 97.4% 82.2%, 92.5% 84.9%, 75.7% 64%, 55.3% 47.5%, 46.5% 49.4%, 45% 62.9%, 50.3% 87.2%, 21.3% 64.1%, 0.1% 100%, 5.4% 51.1%, 21.4% 63.9%, 58.9% 0.2%, 73.6% 51.7%)',
                 }}
               />
             </div>
-            <div className="overflow-hidden">
-              <div className="mx-auto max-w-7xl px-6 pb-32 pt-36 sm:pt-60 lg:px-8 lg:pt-32">
-                <div className="mx-auto max-w-2xl gap-x-14 lg:mx-0 lg:flex lg:max-w-none lg:items-center">
-                  <div className="relative w-full max-w-xl lg:shrink-0 xl:max-w-2xl">
-                    <h1 className="text-gray-900 text-4xl font-bold tracking-tight sm:text-6xl">
-                      We’re changing the way people connect.
-                    </h1>
-                    <p className="text-gray-600 mt-6 text-lg leading-8 sm:max-w-md lg:max-w-none">
-                      Cupidatat minim id magna ipsum sint dolor qui. Sunt sit in
-                      quis cupidatat mollit aute velit. Et labore commodo nulla
-                      aliqua proident mollit ullamco exercitation tempor. Sint
-                      aliqua anim nulla sunt mollit id pariatur in voluptate
-                      cillum.
-                    </p>
-                    <div className="mt-10 flex items-center gap-x-6">
-                      <Link
-                        to={routes.signup()}
-                        className="bg-indigo-600 text-white hover:bg-indigo-500 focus-visible:outline-indigo-600 rounded-md px-3.5 py-2.5 text-sm font-semibold shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2"
-                      >
-                        Get started
-                      </Link>
-                      <Link
-                        to={routes.forgotPassword()}
-                        className="text-gray-900 text-sm font-semibold leading-6"
-                      >
-                        Live demo <span aria-hidden="true">→</span>
-                      </Link>
-                    </div>
-                  </div>
-                  <div className="mt-14 flex justify-end gap-8 sm:-mt-44 sm:justify-start sm:pl-20 lg:mt-0 lg:pl-0">
-                    <div className="ml-auto w-44 flex-none space-y-8 pt-32 sm:ml-0 sm:pt-80 lg:order-last lg:pt-36 xl:order-none xl:pt-80">
-                      <div className="relative">
-                        <img
-                          src="https://images.unsplash.com/photo-1557804506-669a67965ba0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&h=528&q=80"
-                          alt=""
-                          className="bg-gray-900/5 aspect-[2/3] w-full rounded-xl object-cover shadow-lg"
-                        />
-                        <div className="ring-gray-900/10 pointer-events-none absolute inset-0 rounded-xl ring-1 ring-inset" />
-                      </div>
-                    </div>
-                    <div className="mr-auto w-44 flex-none space-y-8 sm:mr-0 sm:pt-52 lg:pt-36">
-                      <div className="relative">
-                        <img
-                          src="https://images.unsplash.com/photo-1485217988980-11786ced9454?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&h=528&q=80"
-                          alt=""
-                          className="bg-gray-900/5 aspect-[2/3] w-full rounded-xl object-cover shadow-lg"
-                        />
-                        <div className="ring-gray-900/10 pointer-events-none absolute inset-0 rounded-xl ring-1 ring-inset" />
-                      </div>
-                      <div className="relative">
-                        <img
-                          src="https://images.unsplash.com/photo-1559136555-9303baea8ebd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&crop=focalpoint&fp-x=.4&w=396&h=528&q=80"
-                          alt=""
-                          className="bg-gray-900/5 aspect-[2/3] w-full rounded-xl object-cover shadow-lg"
-                        />
-                        <div className="ring-gray-900/10 pointer-events-none absolute inset-0 rounded-xl ring-1 ring-inset" />
-                      </div>
-                    </div>
-                    <div className="w-44 flex-none space-y-8 pt-32 sm:pt-0">
-                      <div className="relative">
-                        <img
-                          src="https://images.unsplash.com/photo-1670272504528-790c24957dda?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&crop=left&w=400&h=528&q=80"
-                          alt=""
-                          className="bg-gray-900/5 aspect-[2/3] w-full rounded-xl object-cover shadow-lg"
-                        />
-                        <div className="ring-gray-900/10 pointer-events-none absolute inset-0 rounded-xl ring-1 ring-inset" />
-                      </div>
-                      <div className="relative">
-                        <img
-                          src="https://images.unsplash.com/photo-1670272505284-8faba1c31f7d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&h=528&q=80"
-                          alt=""
-                          className="bg-gray-900/5 aspect-[2/3] w-full rounded-xl object-cover shadow-lg"
-                        />
-                        <div className="ring-gray-900/10 pointer-events-none absolute inset-0 rounded-xl ring-1 ring-inset" />
-                      </div>
-                    </div>
-                  </div>
+            <div className="mx-auto max-w-7xl px-6 pb-24 pt-10 sm:pb-32 lg:flex lg:px-8 lg:py-40">
+              <div className="mx-auto max-w-2xl flex-shrink-0 lg:mx-0 lg:max-w-xl lg:pt-8">
+                <img
+                  className="h-11"
+                  src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
+                  alt="Your Company"
+                />
+                <div className="mt-24 sm:mt-32 lg:mt-16">
+                  <Link to={routes.home()} className="inline-flex space-x-6">
+                    <span className="text-indigo-400 ring-indigo-500/20 rounded-full bg-highlight px-3 py-1 text-sm font-semibold leading-6 ring-1 ring-inset">
+                      What's new
+                    </span>
+                    <span className="text-gray-300 inline-flex items-center space-x-2 text-sm font-medium leading-6">
+                      <span>Just shipped v1.0</span>
+                      <ChevronRightIcon
+                        className="text-gray-500 h-5 w-5"
+                        aria-hidden="true"
+                      />
+                    </span>
+                  </Link>
+                </div>
+                <h1 className="text-white mt-10 text-4xl font-bold tracking-tight sm:text-6xl">
+                  Deploy to the cloud with confidence
+                </h1>
+                <p className="text-gray-300 mt-6 text-lg leading-8">
+                  Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure
+                  qui lorem cupidatat commodo. Elit sunt amet fugiat veniam
+                  occaecat fugiat aliqua.
+                </p>
+                <div className="mt-10 flex items-center gap-x-6">
+                  <Link
+                    to={routes.signup()}
+                    className="text-white focus-visible:outline-highlight rounded-md bg-highlight px-3.5 py-2.5 text-sm font-semibold shadow-sm hover:bg-highlight-lighten focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2"
+                  >
+                    Get started
+                  </Link>
+                  <Link
+                    to={routes.signup()}
+                    className="text-white text-sm font-semibold leading-6"
+                  >
+                    Learn more <span aria-hidden="true">→</span>
+                  </Link>
+                </div>
+              </div>
+              <div className="mx-auto mt-16 flex max-w-2xl sm:mt-24 lg:ml-10 lg:mr-0 lg:mt-0 lg:max-w-none lg:flex-none xl:ml-32">
+                <div className="max-w-3xl flex-none sm:max-w-5xl lg:max-w-none">
+                  <img
+                    src="https://tailwindui.com/img/component-images/dark-project-app-screenshot.png"
+                    alt="App screenshot"
+                    width={2432}
+                    height={1442}
+                    className="bg-white/5 ring-white/10 w-[76rem] rounded-md shadow-2xl ring-1"
+                  />
                 </div>
               </div>
             </div>
           </div>
+          )
         </main>
       </HomeLayout>
     </div>
